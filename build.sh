a9056c2323f40278cdb2e37d6b27502dc2a5671a
Mihai Craita <mcraita@gmail.com>
2021-05-28 14:08:52 +0300

    Build also as a zip archive

--- build.sh
+++ build.sh
@@ -4,8 +4,13 @@ BUILD_FOLDER="build"
 
 mkdir -p $BUILD_FOLDER
 cp -r ./eventbook $BUILD_FOLDER
-composer install --optimize-autoloader --no-dev --working-dir=./$BUILD_FOLDER/eventbook
+composer install -q --optimize-autoloader --no-dev --working-dir=./$BUILD_FOLDER/eventbook
 rm -rf ./$BUILD_FOLDER/eventbook/tests
 rm ./$BUILD_FOLDER/eventbook/phpunit.xml
-tar -zcvf ./$BUILD_FOLDER/eventbook-plugin.tar.gz build/eventbook/ -C ./build
-rm -rf  ./$BUILD_FOLDER/eventbook
+cp ./README.md ./$BUILD_FOLDER/eventbook/.
+echo 'Start building archives'
+cd $BUILD_FOLDER
+zip -qr ./evenbook.zip ./eventbook/*
+tar -zcf ./eventbook-plugin.tar.gz -C ./ ./eventbook
+rm -rf  ./eventbook
+echo 'Build finished'
